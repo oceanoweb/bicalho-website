891fd2595db662f1bcb00192575206d9df53dfba
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-09-27 19:40:13 -0300

    chore: template product

--- src/templates/product.js
+++ src/templates/product.js
@@ -2,15 +2,48 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
-import Title from "../components/Title"
+import ProductsSection from "../components/ProductsSection"
+
+import * as S from "./styled"
 
 const Product = ({ data }) => {
-  const { title, price } = data.markdownRemark.frontmatter
+  const { title, price, image } = data.markdownRemark.frontmatter
+  console.log(data)
+  const productsOfertasList = data.allMarkdownRemark.edges.filter(product =>
+    product.node.frontmatter.category.includes("ofertas")
+  )
 
   return (
     <Layout>
-      <Title title={title} />
-      <Title title={price} />
+      <S.ProductWrapper>
+        <S.ProductCapa />
+        <S.ProductDetailsWrapper>
+          <S.ProductDetails>
+            <S.ProductTitle>{title}</S.ProductTitle>
+            <S.ProductCode>Código XXXXXXXX</S.ProductCode>
+            <S.ProductLabel>Tamanhos disponíveis: </S.ProductLabel>
+            <S.ProductSizeWrapper>
+              <S.ProductSize>2,30</S.ProductSize>
+              <S.ProductSize>2,50</S.ProductSize>
+              <S.ProductSize>2,70</S.ProductSize>
+            </S.ProductSizeWrapper>
+            <S.ProductLabel>A partir de</S.ProductLabel>
+            <S.ProductPrice>{price}</S.ProductPrice>
+            <S.ProductPart>Até 00x de 0000,00 sem juros</S.ProductPart>
+          </S.ProductDetails>
+          <S.ProductButton
+            href={`https://wa.me/5511000000000?text=Olá, gostei do produto ${title}`}
+            target="_blank"
+          >
+            <S.WhatsappIcon />
+            <S.ProductButtonLabel>Comprar</S.ProductButtonLabel>
+          </S.ProductButton>
+        </S.ProductDetailsWrapper>
+        <S.ProducImaageWrapper>
+          <S.ProductImage src={image} />
+        </S.ProducImaageWrapper>
+      </S.ProductWrapper>
+      <ProductsSection title="Confira também" products={productsOfertasList} />
     </Layout>
   )
 }
@@ -24,6 +57,24 @@ export const query = graphql`
         price
         image
         discount
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date] }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            price
+            description
+            image
+            category
+            subcategory
+            discount
+          }
+          fields {
+            slug
+          }
+        }
       }
     }
   }
---
+++ src/templates/styled.js
@@ -0,0 +1,98 @@
+import styled from "styled-components"
+import { Whatsapp } from "@styled-icons/icomoon/Whatsapp"
+
+export const ProductWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.5rem 1.0875rem 1.5rem;
+  margin-bottom: 1rem;
+`
+
+export const ProductCapa = styled.div`
+  background: #f2f2f2;
+  width: 100%;
+  height: 120px;
+  margin-bottom: 1.5rem;
+`
+
+export const ProductDetailsWrapper = styled.div`
+  width: 50%;
+`
+
+export const ProductDetails = styled.div`
+  background: #f2f2f2;
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+`
+
+export const ProductTitle = styled.h1`
+  color: #790d19;
+  text-transform: uppercase;
+  font-size: 1.5rem;
+  font-weight: 900;
+  margin-bottom: 0.2rem;
+`
+
+export const ProductCode = styled.p`
+  font-size: 0.9rem;
+  color: #7f7f7f;
+  margin-bottom: 1rem;
+`
+
+export const ProductLabel = styled.p`
+  font-size: 0.9rem;
+  color: #7f7f7f;
+  margin-bottom: 0.3rem;
+`
+
+export const ProductSizeWrapper = styled.div`
+  display: flex;
+`
+
+export const ProductSize = styled.p`
+  padding: 0.1rem 0.5rem;
+  background: #790d19;
+  color: #fff;
+  border-radius: 15px;
+  font-size: 0.8rem;
+  margin-right: 0.3rem;
+  margin-bottom: 1rem;
+`
+
+export const ProductPrice = styled.p`
+  font-size: 2rem;
+  font-weight: 900;
+  color: #790d19;
+`
+
+export const ProductPart = styled.p`
+  font-size: 0.6rem;
+  color: #7f7f7f;
+`
+
+export const ProductButton = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #25d366;
+  border-radius: 3px;
+  padding: 0.5rem 1rem;
+`
+
+export const WhatsappIcon = styled(Whatsapp)`
+  width: 1rem;
+  color: white;
+  margin-right: 5px;
+`
+
+export const ProductButtonLabel = styled.p`
+  color: white;
+  text-transform: uppercase;
+`
+
+export const ProducImaageWrapper = styled.div`
+  width: 50%;
+`
+export const ProductImage = styled.img``
