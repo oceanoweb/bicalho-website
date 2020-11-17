84a0499948880f7c7936b5586e095244d0c85016
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-11-17 21:49:56 +0000

    chore: update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
         background_color: `#790d19`,
         theme_color: `#790d19`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/bicalho-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
