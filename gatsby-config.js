3017f201925ab04e6996d2876dc805f9ac2b5dee
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2021-02-07 18:44:32 +0000

    feat: add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
